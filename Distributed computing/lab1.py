fcd5e69effa8b9651fef594b8e552ec6b2a53e38
Patrik Peterson <h23patpe@du.se>
2025-12-01 15:24:27 +0100

    fixing Fighting state on Agent

--- Distributed computing/lab1.py
+++ Distributed computing/lab1.py
@@ -1,3 +1,6 @@
+import time
+from enum import nonmember
+
 import spade
 from spade.agent import Agent
 from spade.behaviour import FSMBehaviour, State
@@ -7,13 +10,19 @@ from spade.message import Message
 
 STATE_STANDING = "STATE_STANDING"
 STATE_CASTING = "STATE_CASTING"
+STATE_TREE = "STATE_TREE"
 STATE_WAITING = "STATE_WAITING"
 STATE_BITE = "STATE_BITE"
 STATE_FIGHTING = "STATE_FIGHTING"
+STATE_FIGHTING2 = "STATE_FIGHTING2"
+STATE_FIGHTING3 = "STATE_FIGHTING3"
 STATE_CATCHING = "STATE_CATCHING"
 STATE_LOST = "STATE_LOST"
 STATE_VICTORY = "STATE_VICTORY"
 STATE_DEFEAT = "STATE_DEFEAT"
+STATE_ENDING = "STATE_ENDING"
+STATE_THEEND = "THEEND"
+
 
 class ExampleFSMBehaviour(FSMBehaviour):
     async def on_start(self):
@@ -22,6 +31,8 @@ class ExampleFSMBehaviour(FSMBehaviour):
     async def on_end(self):
         print(f"FSM finished at state {self.current_state}")
         await self.agent.stop()
+
+
 
 class StateStanding(State):
     async def run(self):
@@ -36,11 +47,27 @@ class StateStanding(State):
             self.set_next_state(STATE_STANDING)
 
 
+
 class StateCasting(State):
     async def run(self):
         print("Casting out the lure")
         await asyncio.sleep(5)
-        self.set_next_state(STATE_WAITING)
+        randomnumber = random.random()
+        if randomnumber < 0.1:
+            print("you casted in to a tree and the lure was lost")
+            self.set_next_state(STATE_TREE)
+        else:
+            self.set_next_state(STATE_WAITING)
+
+class StateTree(State):
+    async def run(self):
+        print("do you want to put on a new lure and trying again? yes or no")
+        userinput = input()
+        if userinput == "yes":
+            self.set_next_state(STATE_CASTING)
+        elif userinput == "no":
+            self.set_next_state(STATE_DEFEAT)
+
 
 
 class StateWaiting(State):
@@ -51,50 +78,128 @@ class StateWaiting(State):
 
 class StateBite(State):
     async def run(self):
-        print("Insanely huge fish bites")
+        print("A Fish Bites!")
         await asyncio.sleep(5)
-        self.set_next_state(STATE_FIGHTING)
+        randomnumber = random.random()
+        fish=""
+        if randomnumber < 0.2:
+            print("you have hooked a insanely huge pike")
+            fish="pike"
+            self.set_next_state(STATE_FIGHTING)
+            self.agent.set("fish", fish)
+        elif 0.2 <= randomnumber < 0.5:
+            print("you have hooked a big perch")
+            fish = "perch"
+            self.set_next_state(STATE_FIGHTING)
+            self.agent.set("fish", fish)
+        elif randomnumber > 0.5:
+            print("you have hooked a tiny roach")
+            fish = "roach"
+            self.set_next_state(STATE_FIGHTING)
+            self.agent.set("fish", fish)
+
 
 class StateFighting(State):
     async def run(self):
-        print("incredible fight against 10 kg pike")
-        print("The fish is fighting back hard!!")
+
+        hookedfish = self.agent.get("fish")
+        print("you begin fighting the " + hookedfish)
+        print(f"The fish ${hookedfish} is fighting back hard!!")
         await asyncio.sleep(5)
         # vi använder random module
-
-        print("\nThe fish is trying to escape into the weeds!")
+        # ett event av ovisshet för fiskaren, precis som i riktiga livet.
+        print(f"\nThe {hookedfish} is trying to escape into the weeds!")
         print("Do you want to put MAX PRESSURE on the fish? (yes/no)")
-        print("(YES = 80% chance to catch, but risk of line snap)")
-        print("(NO  = 20% chance to catch, playing it safe but fish might escape)")
+        print("(YES = 50% chance to catch, but risk of line snap)")
+        print("(NO  = 50% chance to catch, playing it safe but fish might escape)")
         #Sparar vi userinput
         userInput = input("Write yes or no: ").lower()
+        randomnumber = random.random()
+        winningword = ""
 
-        lucky_roll = random.random()
-        if userInput == "yes":
-            print("You pull the rod aggresevily! The rod bends like crazy!")
-            await asyncio.sleep(5)
-            if lucky_roll < 0.8:
-                print("It worked! You turned the fish and its super tired, keep reeling!")
-                self.set_next_state(STATE_CATCHING)
+        if randomnumber >= 0.5:
+            winningword = "yes"
 
-            else:
-                print("SNAP!!! The line could not handle the pressure...")
-                self.set_next_state(STATE_LOST)
         else:
-            print("You released the pressure and continues the fight..")
-            await asyncio.sleep(2)
-            if lucky_roll < 0.2:
-                print("You turned the fish and its super tires, keep reeling!")
-                self.set_next_state(STATE_CASTING)
-            else:
-                print("The fish swam away into the grass.. sorry..")
-                print("Do you want to catch more fish?: (yes/no)")
-                userInput = input()
-                if userInput == "yes":
-                    self.set_next_state(STATE_CASTING)
-                else:
-                    exit()
+            winningword = "no"
 
+        if winningword == "yes" and userInput == "yes":
+            print(f"Smart choice friend, putting more pressure on the {hookedfish}")
+            self.agent.set("yes", winningword)
+            await asyncio.sleep(2)
+            self.set_next_state(STATE_FIGHTING2)
+        elif winningword == "no" and userInput == "no":
+            print(f"Coward choice but efficient, releasing pressure on {hookedfish}")
+            self.agent.set("no", winningword)
+            await asyncio.sleep(2)
+            self.set_next_state(STATE_FIGHTING2)
+        else:
+            print("Noooo the fish was lost!!!!")
+            await asyncio.sleep(2)
+            self.set_next_state(STATE_LOST)
+
+
+class StateFighting2(State):
+    async def run(self):
+        hookedfish = self.agent.get("fish")
+        print("ROUND 2")
+        await asyncio.sleep(1)
+        print(f"\nThe {hookedfish} is getting tired and makes a run again!")
+        print("Do you want to put PRESSURE on the fish? (yes/no)")
+        print("(YES = 50% chance to catch, but risk of line snap)")
+        print("(NO  = 50% chance to catch, playing it safe but fish might escape)")
+
+        randomnumber = random.random()
+
+
+        if randomnumber >= 0.5:
+            winningword = "yes"
+        else:
+            winningword = "no"
+
+        userInput = input("Write yes or no: ").lower()
+
+        if winningword == "yes" and userInput == "yes":
+            print("great move! you pulled the fish closer!")
+            await asyncio.sleep(2)
+            self.set_next_state(STATE_FIGHTING3)
+        elif winningword == "no" and userInput == "no":
+            print("Smart patience! The fish stopped running")
+            await asyncio.sleep(2)
+            self.set_next_state(STATE_FIGHTING3)
+        else:
+            print(f"WRONG MOVE! The {hookedfish} jumped and spit the hook!")
+            await asyncio.sleep(2)
+            self.set_next_state(STATE_LOST)
+
+class StateFighting3(State):
+    async def run(self):
+        hookedfish = self.agent.get("fish")
+        print(f"\nROUND 3 FINAL")
+        await asyncio.sleep(1)
+        print(f"The {hookedfish} is by the shore close to us! ")
+        print("Do you want to force it to the net? (yes/no)")
+        userInput = input("Write yes or no: ").lower()
+        randomnumber = random.random()
+        winningword = ""
+
+        if randomnumber >= 0.5:
+            winningword = "yes"
+        else:
+            winningword = "no"
+
+        if winningword == "yes" and userInput == "yes":
+            print("GOTCHA!! Into the net it goes")
+            await asyncio.sleep(2)
+            self.set_next_state(STATE_CATCHING)
+        elif winningword == "no" and userInput == "no":
+            print("Slow and smooth, gently gliding it in")
+            await asyncio.sleep(2)
+            self.set_next_state(STATE_CASTING)
+        else:
+            print("SNAP, oh my god lost it in the end....")
+            await asyncio.sleep(2)
+            self.set_next_state(STATE_LOST)
 
 
 class StateLost(State):
@@ -105,9 +210,12 @@ class StateLost(State):
         self.set_next_state(STATE_DEFEAT)
 
 
+
+
 class StateCatching(State):
     async def run(self):
-        print("Catch personal best pike and cries a little")
+        hookedfish = self.agent.get("fish")
+        print(f"Catch personal best {hookedfish} and cries a little")
         await asyncio.sleep(5)
         self.set_next_state(STATE_VICTORY)
 
@@ -115,18 +223,36 @@ class StateVictory(State):
     async def run(self):
         print("Celebrates catching the fish of a lifetime with a cold beer")
         await asyncio.sleep(5)
+        print("you leave the lake as a happy angler")
+        self.set_next_state(STATE_ENDING)
 
 class StateDefeat(State):
     async def run(self):
         await asyncio.sleep(5)
-        print("you pick up the gun from your pocket")
+        print("you full of sadness and thoughts of selling your'e fishing gear enters your mind")
         await asyncio.sleep(5)
-        print("everything goes black")
+        print("you leave the lake as a broken man")
+        self.set_next_state(STATE_ENDING)
+
+class StateEnding(State):
+    async def run(self):
+        print("do you want to fish the next day as well? write: yes  or are you satsfied for the moement? write: no ")
+        userinput = input()
+        if userinput == "yes":
+            self.set_next_state(STATE_STANDING)
+        elif userinput == "no":
+            self.set_next_state(STATE_THEEND)
+
+class StateTheEnd(State):
+    async def run(self):
+        print("thanks for playing Fishing day")
+
 class FSMAgent(Agent):
     async def setup(self):
         fsm = ExampleFSMBehaviour()
         fsm.add_state(name=STATE_STANDING, state=StateStanding(), initial=True)
         fsm.add_state(name=STATE_CASTING, state=StateCasting())
+        fsm.add_state(name=STATE_TREE, state=StateTree())
         fsm.add_state(name=STATE_WAITING, state=StateWaiting())
         fsm.add_state(name=STATE_BITE, state=StateBite())
         fsm.add_state(name=STATE_FIGHTING, state=StateFighting())
@@ -134,24 +260,40 @@ class FSMAgent(Agent):
         fsm.add_state(name=STATE_VICTORY, state=StateVictory())
         fsm.add_state(name=STATE_LOST, state=StateLost())
         fsm.add_state(name=STATE_DEFEAT, state=StateDefeat())
-
-        #
+        fsm.add_state(name=STATE_ENDING, state=StateEnding())
+        fsm.add_state(name=STATE_THEEND, state=StateTheEnd())
+        fsm.add_state(name=STATE_FIGHTING2, state=StateFighting2())
+        fsm.add_state(name=STATE_FIGHTING3, state=StateFighting3())
         fsm.add_transition(source=STATE_STANDING, dest=STATE_CASTING)
         fsm.add_transition(source=STATE_STANDING, dest=STATE_STANDING)
         fsm.add_transition(source=STATE_CASTING, dest=STATE_WAITING)
+        fsm.add_transition(source=STATE_CASTING, dest=STATE_TREE)
+        fsm.add_transition(source=STATE_TREE, dest=STATE_CASTING)
+        fsm.add_transition(source=STATE_TREE, dest=STATE_DEFEAT)
         fsm.add_transition(source=STATE_WAITING, dest=STATE_BITE)
         fsm.add_transition(source=STATE_BITE, dest=STATE_FIGHTING)
-        fsm.add_transition(source=STATE_FIGHTING, dest=STATE_CATCHING)
+        #fsm.add_transition(source=STATE_FIGHTING, dest=STATE_CATCHING)
         fsm.add_transition(source=STATE_FIGHTING, dest=STATE_LOST)
+        #fsm.add_transition(source=STATE_FIGHTING, dest=STATE_CASTING)
+        fsm.add_transition(source=STATE_FIGHTING, dest=STATE_FIGHTING2)
+        fsm.add_transition(source=STATE_FIGHTING2, dest=STATE_LOST)
+        fsm.add_transition(source=STATE_FIGHTING2, dest=STATE_FIGHTING3)
+        fsm.add_transition(source=STATE_FIGHTING3, dest=STATE_LOST)
+        fsm.add_transition(source=STATE_FIGHTING3, dest=STATE_CATCHING)
+
+
         fsm.add_transition(source=STATE_CATCHING, dest=STATE_VICTORY)
         fsm.add_transition(source=STATE_LOST, dest=STATE_DEFEAT)
+        fsm.add_transition(source=STATE_VICTORY, dest=STATE_ENDING)
+        fsm.add_transition(source=STATE_DEFEAT, dest=STATE_ENDING)
+        fsm.add_transition(source=STATE_ENDING, dest=STATE_STANDING)
+        fsm.add_transition(source=STATE_ENDING, dest=STATE_THEEND)
         self.add_behaviour(fsm)
 
 
 async def main():
     fsmagent = FSMAgent("h23patpe@conversations.im", "Jhgblo10")
     await fsmagent.start()
-
     await spade.wait_until_finished(fsmagent)
     await fsmagent.stop()
     print("Agent finished")
